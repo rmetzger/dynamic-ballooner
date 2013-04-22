db27ec6bc5e98244023627a27c8bbf8d64800664
Robert Metzger <metzgerr@web.de>
2013-04-22 17:51:56 +0200

    The tool is finally working

--- main.cpp
+++ main.cpp
@@ -2,9 +2,19 @@
 #include <stdlib.h>
 #include <proc/sysinfo.h>
 #include <string.h>
+#include <deque>
 #include <queue>
 #include <unistd.h>
 
+/**
+*	Author: Robert Metzger (metzgerr@web.de)
+*
+* The purpose of this tool is to constantly allocate n MB of main memory
+* on a linux machine. 
+* I've used this tool to do I/O hard disk benchmarks and we wanted to have
+* a fixed amount of page cache pages around.
+*
+*/
 
 using namespace std;
 
@@ -21,44 +31,13 @@ void update_m_info() {
 	m_info.mb_cached = kb_main_cached/1024;
 	m_info.mb_buffers = kb_main_buffers/1024;
 	m_info.mb_free = kb_main_free/1024;
-	cout << "Cached: " << m_info.mb_cached << " Buffers: " << m_info.mb_buffers
-		<< " Free: " << m_info.mb_free << endl;
+	//cout << "Cached: " << m_info.mb_cached << " Buffers: " << m_info.mb_buffers
+	//	<< " Free: " << m_info.mb_free << "\r";
 }
 
-template <typename T>
-void debug_queue(const queue<T>* q) {
-	queue<T>* n = new queue<T>(*q);
-
-	int len = q->size();
-	cout << "Len=" << len << endl;
-	for(int i = 0; i < len; ++i) {
-		T f = n->front();
-		cout << "Element " << i << " value " << f << endl;
-		n->pop();
-	}
-}
-
-
-
-// some learning
-void devel() {
-	queue<int>* q = new queue<int>;
-
-	q->push(1);
-	q->push(2);
-	q->push(3);
-	q->push(4);
-	q->push(5);
-
-	debug_queue(q);
-	debug_queue(q);
-}
 
 int main(int argc, char* argv[])
 {
-	// devel();
-	// return 0;
-
 	// info from /proc/meminfo
 	m_info.mb_cached = 0;
 	m_info.mb_buffers = 0;
@@ -66,8 +45,7 @@ int main(int argc, char* argv[])
 	// the amount of mem the user wants for the cache.
 	int desiredCacheSize = -1;
 
-	queue<void*>*  allocated_memory = new queue<void*>;
-
+	deque<void*>*  allocated_memory = new deque<void*>;
 
 	if(argc <= 1) {
 		cout << "Usage: ballooner <desiredCacheSizeMegaBytes>" << endl;
@@ -77,41 +55,55 @@ int main(int argc, char* argv[])
 			cout << "Values equal or lower to zero are not supported";
 			return 1;
 		}
-		cout << "desiredCacheSize="<<desiredCacheSize<< endl;
 
 		update_m_info();
-		int set = 0;
-		while(true) {
-			cout<< "Currently are " << m_info.mb_cached << "MB in the page cache" << endl;
 
-			if(m_info.mb_cached > desiredCacheSize + m_info.mb_buffers) {
+		int alloc_count = 0;
+		int alloc_sum = 0;
+		int avg = -1;
+		while(true) {
+			if(m_info.mb_free+m_info.mb_cached > desiredCacheSize) {
+
 				// we need to allocate memory
-				int alloc_mb = (m_info.mb_cached+m_info.mb_free)/2;
-				if(alloc_mb > desiredCacheSize) {
-					alloc_mb = alloc_mb-desiredCacheSize;
+				int alloc_mb = (m_info.mb_cached+m_info.mb_free)/50; // edit this param for finer alloc granularity
+				if(avg != -1 && alloc_mb < avg) {
+					usleep(5000); // sleep for 0.005 seconds
+					goto endofLoop;
 				}
 				long amount = (long)alloc_mb*1024*1024;
 				cout << "Allocating " << alloc_mb << " MB " << endl;
 				void * buffer =  malloc ( amount );
-				cout << "buffer?? " << buffer << endl;
-				memset(buffer, set++, amount);
-				allocated_memory->push(buffer);
-				cout << "Debugging queue" << endl;
-				debug_queue(allocated_memory);
+				memset(buffer, 1, amount);
+				allocated_memory->push_front(buffer);
+
+				if(avg == -1) {
+					alloc_count++;
+					alloc_sum += alloc_mb;
+					cout << "Avg: " << alloc_sum/alloc_count << endl;
+				}
+				if(alloc_count == 200) { // set this parameter higher, to have a more accurate "sleep phase"
+					avg = alloc_sum/alloc_count;
+				}
 			} else {
 				// free up again.
+				if(allocated_memory->size() == 0) {
+					cout << "No memory allocated!" << endl;
+					goto endofLoop;
+				} 
 				void* somemem = allocated_memory->back();
+				allocated_memory->pop_back();
 				if(somemem != NULL) {
+					cout << "Freeing " << somemem << endl;
 					free(somemem);
 				} else {
 					cout << "Can not free more memory! Exiting...";
 					return 0;
 				}
 			}
-			sleep(4); // don't stress the kernel too much
+			endofLoop:
+			// sleep(1); // don't stress the kernel too much
+
 			update_m_info();
 		}
 	}
 }
-
-
